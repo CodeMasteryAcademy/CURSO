b5fd0f9eac7b01d90e0da38ece1068a92e8cd6b7
CodeMastery Academy <151575394+CodeMasteryAcademy@users.noreply.github.com>
2023-12-30 17:51:58 +0000

    feito

--- HNY_app/js/script.js
+++ HNY_app/js/script.js
@@ -4,8 +4,8 @@ let newSentenceList = '';
 let sentenceList = [
     "Desperte o brilho dentro de você e ilumine o mundo ao seu redor.",
     "Que os fracassos desse ano sejam os degraus necessários para atingir tudo que você almeja no próximo ano!",
-    "Transforme bugs em oportunidades de aprendizado e faça do código fonte da vida uma obra-prima motivadora.",
-    "Você ainda pode virar o jogo",
+    "Se és programador, transforme bugs em oportunidades de aprendizado e faça do código fonte da vida uma obra-prima motivadora.",
+    "Você ainda pode virar o jogo.",
     "Seja bem ao futuro, o que vai desejar para esse ano de 2024?"
 ];
 
