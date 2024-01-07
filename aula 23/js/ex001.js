5c0dc0a51f9a4116c96b404e2c31ff4f0f8912d2
CodeMastery Academy <151575394+CodeMasteryAcademy@users.noreply.github.com>
2024-01-07 16:31:06 +0000

    .

---
+++ aula 23/js/ex001.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let meuArray = [];
+
+const num1 = Number(prompt('Digite um número: '));
+const num2 = Number(prompt('Digite outro número: '));
+const soma = num1 + num2;
+
+meuArray.push(num1, num2, soma);
+console.log(meuArray);
+
+
---
+++ aula 23/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let vetor = ["Mário", 17, true];
+/*
+
+    unshif() -> adicionar na primeira posição
+    shift() -> eliminar na primeira posição
+
+    push() -> adicionar na última posição
+    pop() -> eliminar na última posição
+
+    splice() -> eliminar um elemento em específico
+
+    sort() -> organizar o meu vetor
+
+*/
+
+
+/*
+    As elementos de um objeto:
+        métodos: funções
+        propriedades: valores
+
+    As formas de acesso:
+        dot notation: .
+        bracket notation: []
+*/
+
+
+const pessoa = {
+    nome: "Mário",
+    idade: 17, 
+    estadoCivil: "solteiro",
+    saudar: function saudar() {
+        console.log("Olá amigo!");
+    }
+};
+
+
+
+
